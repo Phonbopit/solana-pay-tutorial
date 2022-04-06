05d3c0e764dc28293fa033b0daf0dcb9230e7bee
Chai Phonbopit <phonbopit@gmail.com>
2022-04-06 21:34:59 +0700

    initial

---
+++ lib/addresses.ts
@@ -0,0 +1,14 @@
+import { PublicKey } from '@solana/web3.js'
+
+// Your shop wallet address
+export const shopAddress = new PublicKey(
+  '3CVqKDt6xZixtfvdq1c85WbkNTgiABGjwQMsTkzUNL34'
+)
+
+export const usdcAddress = new PublicKey(
+  'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr'
+)
+
+export const couponAddress = new PublicKey(
+  'DKxTwFAk2KspXgYP8PVM3FGuXQEYrvXC7urT5rBAbFJG'
+)
